27f317d05476aacfd28292287d1043693c48463c
amritachaudri <38041057+amritachaudri@users.noreply.github.com>
2020-04-03 16:41:02 +0530

    Using label encoder

--- code.py
+++ code.py
@@ -14,7 +14,21 @@ print(df.describe())
 features=df.columns[:22]
 print(features)
 features=df[features]
+
 #Mapping the grade feature 
 map1={'O':10,'A+':9,'A':8,'B+':7,'B':6,'C':5,'D':4,'E':3,'F':2}
 features['Grade']=features['Grade'].map(map1)
 print(features['Grade'])
+
+#Label Encoder
+#the word labels are converted into number Label
+from sklearn.preprocessing import LabelEncoder
+le=LabelEncoder()
+features['Course']=le.fit_transform(features['Course'])
+features['MHRDName']=le.fit_transform(features['MHRDName'])
+features['ScholarType']=le.fit_transform(features['ScholarType'])
+features['Direction']=le.fit_transform(features['Direction'])
+features['Gender']=le.fit_transform(features['Gender'])
+features['Medium']=le.fit_transform(features['Medium'])
+features['CourseType']=le.fit_transform(features['CourseType'])
+features['ProgramType']=le.fit_transform(features['ProgramType'])
