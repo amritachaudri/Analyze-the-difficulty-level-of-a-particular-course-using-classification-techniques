14e610ed442d4b2be6705460b11d0bd7e8f256a2
amritachaudri <38041057+amritachaudri@users.noreply.github.com>
2020-04-03 16:38:27 +0530

    Analyzing the data-set

--- code.py
+++ code.py
@@ -2,3 +2,15 @@
 import pandas as pd
 df=pd.read_csv(r'C:\Users\Harprem Singh Chaudr\Documents\btech_data.csv')
 print(df.head(5))
+#Understanding the dataset
+print("Checking the size of the file")
+print(df.size)
+print("The number of rows are")
+print(df.shape[0])
+print("The number of columns are")
+print(df.shape[1])
+print("Viewing the detailed data")
+print(df.describe())
+features=df.columns[:22]
+print(features)
+features=df[features]
