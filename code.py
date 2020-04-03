b9cbc82130c2cdcaeb39f591b10d9c1acda0bc1e
amritachaudri <38041057+amritachaudri@users.noreply.github.com>
2020-04-03 16:35:38 +0530

    importing dataset

---
+++ code.py
@@ -0,0 +1,4 @@
+#import the dataset that contains the data of the students in biotechnogoly
+import pandas as pd
+df=pd.read_csv(r'C:\Users\Harprem Singh Chaudr\Documents\btech_data.csv')
+print(df.head(5))
